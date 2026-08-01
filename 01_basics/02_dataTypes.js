1c4029bf213da9008627b9328a029f1439bdcc23
Richa-Singh-Codes <143276040+Richa-Singh-Codes@users.noreply.github.com>
2026-08-01 02:56:40 +0000

    DataTypes of Js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // Means- Treat all JS code as newer version of JS
+
+// alert( 3 + 3) --> We are using nodeJs, not browser
+
+let name = "mahi"
+let age = 19
+let isLoggedIn = false
+
+
+/*
+
+number --> 2 to the powe 53
+bigint
+string --> ""
+boolean --> true/false
+null --> standalone value
+undefined --> value is not assigned
+symbol --> for uniqueness
+
+ */
+
+
+// Object
+console.log(typeof undefined)  // undefined
+console.log(typeof null);     // object
